653d0b3a44ef8cd7d3d9db04e72dfe501572baf1
shivitatiwari <shivitatiwari@gmail.com>
2026-09-18 16:09:11 +0530

    Pin Compose to API 36 compatible stable line [release]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ kotlin {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2026.08.00")
+    val composeBom = platform("androidx.compose:compose-bom:2026.04.01")
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
