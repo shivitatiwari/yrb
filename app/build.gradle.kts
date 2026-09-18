8aca29edac3079df7027da5575f90e122e344a03
shivitatiwari <shivitatiwari@gmail.com>
2026-09-18 19:26:10 +0530

    Bump Yrb to v1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.apoorv.yrb"
         minSdk = 30
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4.0"
+        versionCode = 6
+        versionName = "1.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
