d1433c2b50ecd53571a83f4fb5c2a23f44103160
shivitatiwari <shivitatiwari@gmail.com>
2026-09-18 21:35:15 +0530

    Bump Yrb to v1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.apoorv.yrb"
         minSdk = 30
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.6.0"
+        versionCode = 8
+        versionName = "1.7.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
