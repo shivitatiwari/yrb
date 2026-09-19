a7ac62f63317a4ba4ce252a1bc2c37a90601440d
shivitatiwari <shivitatiwari@gmail.com>
2026-09-19 08:53:04 +0530

    fix: import local vector painter resources

--- app/src/main/java/com/apoorv/yrb/MainActivity.kt
+++ app/src/main/java/com/apoorv/yrb/MainActivity.kt
@@ -64,6 +64,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
